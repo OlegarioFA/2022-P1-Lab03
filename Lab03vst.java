7ca50c1b46d652e10c03cb5fed6add4fa48c0016
Olegario <olegario.fernandezalonso@vtsd.com>
2022-09-19 10:02:15 -0400

    Code update

--- Lab03vst.java
+++ Lab03vst.java
@@ -9,7 +9,18 @@ public class Lab03vst
 	public static void main(String[] args)
 	{
       System.out.println("Lab03, 80 Point Version\n");
-      
+
+	  int sec= 10000;
+	  int hours= sec/ 3600;
+	  int sec1= sec%3600;
+	  int min= sec1/60;
+	  int secmin= sec1%60;
+
+	  System.out.println("Starting Seconds:  " + sec);
+	  System.out.println("Hours:  " + hours);
+	  System.out.println("Minutes:  " + min);
+	  System.out.println("Seconds:  " + secmin);
+
 	}
 }
 
