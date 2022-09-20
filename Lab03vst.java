750bb1fb9d52e91735202bacc1a3fd8ccd12dd43
Olegario <olegario.fernandezalonso@vtsd.com>
2022-09-20 08:59:37 -0400

    Code update

--- Lab03vst.java
+++ Lab03vst.java
@@ -4,23 +4,21 @@
 // Resave this program as Lab03v100 for the 100 point version.
 
 
-public class Lab03vst
-{
-	public static void main(String[] args)
-	{
-      System.out.println("Lab03, 80 Point Version\n");
+public class Lab03vst {
+    public static void main(String[] args) {
+        System.out.println("Lab03, 80 Point Version\n");
 
-	  int sec= 10000;
-	  int hours= sec/ 3600;
-	  int sec1= sec%3600;
-	  int min= sec1/60;
-	  int secmin= sec1%60;
+        int sec = 10000;
+        int hours = sec / 3600;
+        int secl = sec % 3600;
+        int min = secl / 60;
+        int secmin = secl % 60;
 
-	  System.out.println("Starting Seconds:  " + sec);
-	  System.out.println("Hours:  " + hours);
-	  System.out.println("Minutes:  " + min);
-	  System.out.println("Seconds:  " + secmin);
+        System.out.println("Starting Seconds:  " + sec);
+        System.out.println("Hours:  " + hours);
+        System.out.println("Minutes:  " + min);
+        System.out.println("Seconds:  " + secmin);
 
-	}
+    }
 }
 
