6c165e33fe7cb8ad5252c5025a52ffde022bb9c8
Olegario <olegario.fernandezalonso@vtsd.com>
2022-09-21 08:33:47 -0400

    code update

---
+++ Casting.java
@@ -0,0 +1,19 @@
+public class Casting {
+
+    public static void main(String args[]) {
+
+
+        int dogs = 4;
+        double exactDogs = 7.2;
+        char nicksFav = 'S';
+
+        System.out.println(dogs);
+        System.out.println(exactDogs);
+        System.out.println((int)nicksFav);
+        nicksFav++;
+        System.out.println(nicksFav);
+
+        boolean sadness = false;
+        System.out.println(sadness);
+    }
+}
